a4e638c44244be10d70ab8c20a67b5308329e4b2
brendanjamesmulhern <brendanjamesmulhern@gmail.com>
2022-04-01 14:42:56 -0400

    deploying

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace MvcApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index();
+        {
+            return View();
+        }
+    }
+}
